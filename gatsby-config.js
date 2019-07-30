44ca12eb79d700488d6a86da02bef5d1f15e6b1d
Mustafa Ekrem KENTER <mkenter@thy.com>
2019-07-30 23:13:05 +0300

    initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,34 +1,35 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Mustafa Ekrem KENTER`,
+    description: `Software Craftsmen`,
+    author: `@ekremkenter`
   },
   plugins: [
+    `gatsby-plugin-material-ui`,
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
-        path: `${__dirname}/src/images`,
-      },
+        path: `${__dirname}/src/images`
+      }
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Mustafa Ekrem KENTER Personal Web Site`,
+        short_name: `ekremkenter.com`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#37474f`,
+        theme_color: `#cfd8dc`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-      },
-    },
+        icon: `src/images/profile_crop_1024.jpg` // This path is relative to the root of the site.
+      }
+    }
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
-  ],
-}
+  ]
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,96 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from "react";
+import SEO from "../components/seo";
+import { colors, Tooltip, Typography } from "@material-ui/core";
+import { makeStyles } from "@material-ui/styles";
+import { IconContext } from "react-icons";
+import { FaChartBar, FaGithub, FaLinkedin, FaTwitter } from "react-icons/fa";
+import profilePhoto from "../images/profile_crop_1024.jpg";
+import Avatar from "@material-ui/core/Avatar";
+import "../components/base.css";
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+const useStyles = makeStyles({
+  root: {
+    display: "flex",
+    width: "100vw",
+    textAlign: "center",
+    height: "100vh",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: colors.blueGrey["800"],
+    color: colors.blueGrey[100]
+  },
+  iconBar: {
+    marginTop: "5vh"
+  },
+  social: {
+    display: "inline",
+    color: colors.blueGrey["100"],
+    transition: "color 1s ease",
+    marginLeft: "1rem",
+    width: "2rem",
+    height: "2rem",
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+    "&:hover": {
+      color: "white",
+      cursor: "pointer"
+    }
+  },
+  avatar: {
+    width: "12rem",
+    height: "12rem",
+    marginBottom: "5vh"
+  }
+});
+
+const IndexPage = () => {
+  const classes = useStyles();
+  return (
+    <div className={classes.root}>
+
+      <SEO title="Mustafa Ekrem KENTER"/>
+
+      <Avatar alt="Mustafa Ekrem KENTER" src={profilePhoto} className={classes.avatar}/>
+      <Typography gutterBottom variant="h3">Hello, I'm Ekrem</Typography>
+      <Typography gutterBottom variant="h4">An experienced software craftsman</Typography>
+
+      <div className={classes.iconBar}>
+        <IconContext.Provider value={{ className: classes.social }}>
+          <a
+            href="https://linkedin.com/in/ekremkenter"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            <FaLinkedin/>
+          </a>
+          <a
+            href="https://github.com/ekremkenter"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            <FaGithub/>
+          </a>
+          <a
+            href="https://twitter.com/ekremkenter"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            <FaTwitter/>
+          </a>
+          <a
+            href="https://profile.codersrank.io/user/ekremkenter"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            <Tooltip title="codersrank.io">
+              <FaChartBar/>
+            </Tooltip>
+          </a>
+        </IconContext.Provider>
+      </div>
+
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+  );
+};
 
-export default IndexPage
+export default IndexPage;

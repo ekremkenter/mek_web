b51bd7ecbe2fb61744727537b8541949aae8b689
Mustafa Ekrem KENTER <ekremkenter@gmail.com>
2020-03-21 14:01:21 +0300

    add current cv redirect page

---
+++ src/pages/cv.js
@@ -0,0 +1,23 @@
+import React, { useEffect } from "react";
+import SEO from "../components/seo";
+import { CircularProgress, colors, Tooltip, Typography } from "@material-ui/core";
+import { makeStyles } from "@material-ui/styles";
+import { IconContext } from "react-icons";
+import { FaChartBar, FaGithub, FaLinkedin, FaTwitter } from "react-icons/fa";
+import {OutboundLink} from "gatsby-plugin-google-analytics";
+
+import profilePhoto from "../images/profile.jpg";
+import saa from "../images/aws_saa.png";
+import Avatar from "@material-ui/core/Avatar";
+import "../components/base.css";
+
+const IndexPage = () => {
+  useEffect(function() {
+    window.location = "https://drive.google.com/file/d/1CBxiArbdkqXokBUZshIMiMfZN5Mzc-6R/view?usp=sharing"
+  });
+  return (
+    <CircularProgress/>
+  );
+};
+
+export default IndexPage;
